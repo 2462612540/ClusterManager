d518db2ec00661fa1f1be61c68be63b9e5ec7ff6
xjl <18279148786@163.com>
2021-08-22 14:46:09 +0800

    构建系统SRS系统

---
+++ Cluster-Manager-Parent/Cluster-Manager-SRS/src/test/java/cluster/manager/srs/SrsApplicationTests.java
@@ -0,0 +1,13 @@
+package cluster.manager.srs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
